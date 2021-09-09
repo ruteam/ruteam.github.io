561ec107ac584fff6675e2e00ff8f791cc5104d3
Putean <71219363+Putean@users.noreply.github.com>
2021-09-09 21:58:29 +0300

    09.09.2021

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Счетчик кол-во товаров
+let k = 1;
+document.querySelector(".count__item").innerHTML = k;
+let d = 0;
+let plus = document.querySelector(".plus");
+let minus = document.querySelector(".min");
+
+plus.onclick = function() {
+	if (k <= 8) {
+		k = k + 1;
+		document.querySelector(".count__item").innerHTML = k;
+	}
+
+}
+
+minus.onclick = function() {
+	if (k >= 2) {
+		k = k - 1;
+		document.querySelector(".count__item").innerHTML = k;
+	}
+}
+
+// Уведомление о покупке
+let a = document.querySelector(".tovar-btn");
+a.onclick = function () {
+	alert("Спасибо за покупку! Приходите еще!");
+}
